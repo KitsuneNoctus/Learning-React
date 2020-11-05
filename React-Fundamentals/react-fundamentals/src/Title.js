a87ec0fa2c1565c2574d5ac3423892c7f877b627
Henry Calderon <theceintiant@gmail.com>
2020-11-04 23:57:53 -0800

    Header component built

---
+++ React-Fundamentals/react-fundamentals/src/Title.js
@@ -0,0 +1,20 @@
+// src/Title.js
+import './Title.css';
+
+import React from "react"
+
+
+function Title() {
+  return (
+    <div className='Title'>
+      <header>
+        <h1>SFPOPOS</h1>
+        <div className="Title-Subtitle">
+            <p>San Francisco Privately Owned Public Open Spaces</p>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Title
